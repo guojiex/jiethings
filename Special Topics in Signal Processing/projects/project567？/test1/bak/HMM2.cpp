11a021e3a1bef7c6780a076030964817a6abd0d7
guoskyhero <1036479561@qq.com>
2015-07-11 10:28:10 +0800

    reverse
    Signed-off-by: guoskyhero <1036479561@qq.com>

--- Special Topics in Signal Processing/projects/project567？/test1/bak/HMM2.cpp
+++
@@ -1,9 +0,0 @@
-#include "HMM2.h"
-#include "DTW.h"
-#include <assert.h>
-#include "Segment.h"
-HMM2::HMM2( DTW input,int st):states(st)
-{
-	int a[]={0,1,2,3};
-	for(int )
-}
--- Special Topics in Signal Processing/projects/project567？/test1/mathUtil.h
+++
@@ -1,5 +0,0 @@
-#include <vector>
-double vectorEuclideanDistance(const std::vector<double> &x,const std::vector<double> &y);
-std::string createTemplatePath(int sampleNumber,int index);
-std::string getstring ( const int n );
-std::vector<std::vector<double>> readDeltaFromWavFile(std::string fileName);
